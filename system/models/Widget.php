526707f87a1683b1f04a4575dd7d1cc4b0be002d
thenineties <68155152+thenineties@users.noreply.github.com>
2020-10-13 20:34:09 +0545

    Update Widget.php

--- system/models/Widget.php
+++ system/models/Widget.php
@@ -6,7 +6,7 @@ class Widget extends Model
     public static function defaultWidget($key)
     {
         $widgets = [
-            'client-auth-page-widget' => '<h2>Welcome to CloudOnex</h2>
+            'client-auth-page-widget' => '<h2>Welcome to Eduwise Accounting</h2>
 <p>In this widget, you can add a notice, CTA, or any content you want. This widget only shows here on the client portal page. Only admin can edit this widget. If you are logged in as admin, you will see an edit button above. If you do not see the toolbar above, <a id="login_as_admin" href="javascript:;">log in as admin</a> and revisit this page.</p>
 <p>To change the above logo, login as admin then go to customize under appearance.</p>
 <p>The footer brand name comes from your company name. Edit the name from general settings under Settings.</p>',
