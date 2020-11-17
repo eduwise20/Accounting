95da54947a4ad73488fa199c5aae0ff9a65dfb8a
thenineties <68155152+thenineties@users.noreply.github.com>
2020-11-17 11:59:37 +0545

    Delete config.php

--- system/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_HOST', 'localhost'); # Database Host
-define('DB_PORT', ''); # Database Port. Keep it blank if you are un sure.
-define('DB_USER', 'root'); # Database Username
-define('DB_PASSWORD', 'shrijana'); # Database Password
-define('DB_NAME', 'finance'); # Database Name
-define('APP_URL', 'http://localhost/accounting'); # Application URL.
-#Please include http and do not use trailing slash after the url. For example use in this format- http://www.example.com Or http://www.example.com/finance
-define('APP_STAGE', 'Dev'); # Change Live to Dev to enable Debug
