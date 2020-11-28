e4d64808658fc98d8372ce87a2dbe5cf91b503ab
shrijana <shrijana1991@gmail.com>
2020-11-28 18:04:46 +0545

    add quote in int

--- apps/categories/install.php
+++ apps/categories/install.php
@@ -7,7 +7,7 @@ $table->add('updated_at', 'TIMESTAMP', '', 'null');
 $table->save(); 
 
 $table = new Schema('zzz_6_subcategories');
-$table->add('category_id', int, 11);
+$table->add('category_id', 'int', 11);
 $table->add('name', 'varchar', 256);
 $table->add('remarks', 'varchar', 256);
 $table->add('created_at', 'TIMESTAMP', '', 'null');
