f800d6e1af003c0410113abc36147d6deb5c9be1
Sanjay Maharjan <sanjay.invents@gmail.com>
2020-12-03 01:53:54 +0545

    [sm_assign_discount_to_student] : Added assign discount to student module.

---
+++ apps/assign_discount_to_students/models/AppDiscountStudent.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class AppDiscountStudent extends Model {
+
+    protected $table = 'fee_structure_student_scholarship';
+
+}
---
+++ apps/assign_discount_to_students/uninstall.php
@@ -0,0 +1,4 @@
+
+<?php
+$billing_periods = new Schema('fee_structure_student_discount');
+$billing_periods->drop();
